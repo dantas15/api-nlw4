13ce9f9a98a5f6ea367ef68c7953f985fcd2b9e9
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-02-28 17:01:46 -0300

    Cálculo do NPS e validação do User

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -31,32 +31,40 @@ class SendMailController {
             });
         }
         
+        
+        const npsPath = resolve(__dirname, "../", "views", "emails", "npsMail.hbs");
+        
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: { user_id: user.id, value: null},
+            relations: ["user", "survey"],
+        });
+
         const variables = {
             name: user.name,
             title: survey.title,
             description: survey.description,
-            user_id: user.id,
+            id: "",
             link: process.env.URL_MAIL,
-        }
-
-        const npsPath = resolve(__dirname, "../", "views", "emails", "npsMail.hbs");
-        
-        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-            where: [{ user_id: user.id}, {value: null}],
-            relations: ["user", "survey"],
-        });
+        };
 
         if(surveyUserAlreadyExists) {
+            variables.id = surveyUserAlreadyExists.id;
+
             await SendMailService.execute(email, survey.title, variables, npsPath);
             return response.json(surveyUserAlreadyExists);
         }
+
+        // Salvar as informações na tabela surveyUser
         const surveyUser = surveysUsersRepository.create({
            user_id: user.id,
-           survey_id: survey.id
+           survey_id,
         });
 
         await surveysUsersRepository.save(surveyUser);
-        
+
+        // Enviar e-mail para o usuário
+        variables.id = surveyUser.id;
+         
         await SendMailService.execute(email, survey.title, variables, npsPath);
 
         return response.json(surveyUser);
